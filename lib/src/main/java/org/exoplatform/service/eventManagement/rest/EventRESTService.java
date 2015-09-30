821a858743d7fc1b60f832d01e1cc0920f92dae7
lneji <lneji@exoplatform.com>
2015-09-30 17:54:52 +0100

    Preparing nodetype and services ..

---
+++ lib/src/main/java/org/exoplatform/service/eventManagement/rest/EventRESTService.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright (C) 2014 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.exoplatform.service.taskManagement.rest;
+
+import org.exoplatform.commons.utils.ListAccess;
+import org.exoplatform.service.taskManagement.entities.Project;
+import org.exoplatform.service.taskManagement.service.ProjectService;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.exoplatform.services.organization.OrganizationService;
+import org.exoplatform.services.organization.User;
+import org.exoplatform.services.rest.resource.ResourceContainer;
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ * @author <a href="mailto:foo@bar.org">Foo Bar</a>
+ * @version $Id: Body Header.java 34027 2009-07-15 23:26:43Z aheritier $
+ */
+
+
+/*
+ * Copyright (C) 2003-2014 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+/**
+ * Created by The eXo Platform SAS
+ * Author : eXoPlatform
+ *          exo@exoplatform.com
+ * Jun 26, 2014
+ */
+@Path("/taskmanagement/project")
+public class ProjectRESTService implements ResourceContainer
+{
+   private ProjectService projectService;
+   private OrganizationService organizationService;
+   private static final Log LOG = ExoLogger.getLogger("taskManagement.service.ProjectRESTService");
+
+   public ProjectRESTService(OrganizationService organizationService, ProjectService projectService)
+   {
+
+      this.organizationService = organizationService;
+      this.projectService = projectService;
+   }
+
+   @GET
+   @Path("/addproject")
+   @Produces(MediaType.TEXT_HTML)
+   @RolesAllowed("users")
+   public Response createProject(@Context SecurityContext sc,
+                                 @Context UriInfo uriInfo,@QueryParam("name") String name, @QueryParam("lead") String lead)
+   {
+      Project proj = projectService.addProject(name, lead);
+      return Response.ok(proj.toString(),MediaType.APPLICATION_JSON).build();
+   }
+
+   @GET
+   @Path("/getalluser")
+   @Produces("application/json")
+   @RolesAllowed("users")
+   public Response getUser()
+   {
+      JSONArray jsonArray = new JSONArray();
+      JSONObject jsonGlobal = new JSONObject();
+      try
+      {
+         ListAccess<User> listUsers = organizationService.getUserHandler().findAllUsers();
+         for (User user : listUsers.load(0, listUsers.getSize()))
+         {
+            System.out.println(user.getFullName());
+            JSONObject json = new JSONObject();
+            json.put("username", user.getFullName());
+            jsonArray.put(json);
+         }
+         jsonGlobal.put("totalNbUsers", listUsers.getSize());
+         jsonGlobal.put("projects", jsonArray);
+
+      }
+      catch (Exception e)
+      {
+         LOG.error(e);
+      }
+      return Response.ok(jsonGlobal.toString(), MediaType.APPLICATION_JSON).build();
+   }
+
+
+   @GET
+   @Path("/getallprojects")
+   @Produces("application/json")
+   @RolesAllowed("users")
+   public Response getAllProjects()
+   {
+
+      JSONArray jsonArray = new JSONArray();
+      JSONObject jsonGlobal = new JSONObject();
+
+      try
+      {
+         for (Project project : projectService.getAllProject())
+         {
+            JSONObject json = new JSONObject();
+            json.put("projectId", project.getId());
+            json.put("nameProject", project.getName());
+            json.put("teamLead", project.getLead());
+            jsonArray.put(json);
+         }
+         jsonGlobal.put("totalNbProjects", projectService.getAllProject().size());
+         jsonGlobal.put("projects", jsonArray);
+      }
+      catch (JSONException e)
+      {
+         LOG.error(e);
+      }
+
+      return Response.ok(jsonGlobal.toString(), MediaType.APPLICATION_JSON).build();
+
+   }
+
+
+}
---
+++ lib/src/main/java/org/exoplatform/service/eventManagement/service/EventService.java
@@ -0,0 +1,231 @@
+/*
+ * Copyright (C) 2014 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.exoplatform.service.taskManagement.service;
+
+import org.exoplatform.service.taskManagement.entities.Project;
+import org.exoplatform.service.taskManagement.entities.Task;
+import org.exoplatform.services.jcr.RepositoryService;
+import org.exoplatform.services.jcr.core.ManageableRepository;
+import org.exoplatform.services.jcr.ext.common.SessionProvider;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.query.Query;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * @author <a href="mailto:foo@bar.org">Foo Bar</a>
+ * @version $Id: Body Header.java 34027 2009-07-15 23:26:43Z aheritier $
+ */
+public class ProjectService
+{
+   private String taskRootNode = "TaskManagement";
+   private RepositoryService repositoryService;
+   private static final Log LOG = ExoLogger.getLogger("taskManagement.service.ProjectService");
+
+   public ProjectService(RepositoryService repositoryService)
+   {
+      this.repositoryService = repositoryService;
+   }
+
+
+   public Project addProject(String pName, String teamLead)
+   {
+      Project pr = null;
+      Node root = null;
+      try
+      {
+         root = getTaskRootNode();
+      }
+      catch (RepositoryException e)
+      {
+         LOG.error(e);
+      }
+      try
+      {
+         long newId = System.currentTimeMillis();
+         Node node = root.addNode("Project" + newId, "exo:project");
+         node.setProperty("idProject", newId);
+         node.setProperty("teamLead", teamLead);
+         node.setProperty("nameProject", pName);
+         root.save();
+         pr = new Project((int)newId, pName, teamLead);
+      }
+      catch (Exception e)
+      {
+         LOG.error(e);
+      }
+      return pr;
+   }
+
+   public List<Project> getAllProject()
+   {
+      List<Project> list = new ArrayList<Project>();
+      Node root = null;
+      try
+      {
+         root = getTaskRootNode();
+      }
+      catch (RepositoryException e)
+      {
+         e.printStackTrace();
+      }
+      String statement = null;
+      try
+      {
+         statement = "SELECT * FROM  exo:project WHERE jcr:path LIKE '" + root.getPath() + "/%'";
+         Query query = root.getSession().getWorkspace().getQueryManager().createQuery(statement, Query.SQL);
+         for (NodeIterator iter = query.execute().getNodes(); iter.hasNext(); )
+         {
+            Node n = iter.nextNode();
+            long id = n.hasProperty("idProject")? n.getProperty("idProject").getLong():0;
+            String name = n.hasProperty("nameProject")?n.getProperty("nameProject").getString():"";
+            String lead = n.hasProperty("teamLead")? n.getProperty("teamLead").getString():"";
+            Project pr = new Project(id, name, lead);
+            list.add(pr);
+         }
+      }
+      catch (RepositoryException e)
+      {
+         LOG.error(e);
+      }
+
+      return list;
+   }
+
+   public List<Task> getTasks(long projectId)
+   {
+
+      List<Task> list = new ArrayList<Task>();
+      Node pNode = getProjectNodeById(projectId);
+      try
+      {
+         for (NodeIterator iter = pNode.getNode("tasksList").getNodes(); iter.hasNext(); )
+         {
+            Task task = new Task();
+            Node n = iter.nextNode();
+            if(pNode.hasProperty("idProject"))task.setProjectId(pNode.getProperty("idProject").getLong());
+            if(n.hasProperty("idTask"))task.setId(n.getProperty("idTask").getLong());
+            if(n.hasProperty("description"))task.setDescription(n.getProperty("description").getString());
+            if(n.hasProperty("nameTask"))task.setName(n.getProperty("nameTask").getString());
+            //task.setDueDate(n.getProperty("idTask").getDate());
+            if(n.hasProperty("affected"))task.setAffected(n.getProperty("affected").getString());
+            list.add(task);
+         }
+      }
+      catch (RepositoryException e)
+      {
+         LOG.error(e);
+      }
+      return list;
+
+   }
+
+   public Node getProjectByName(String projectName) throws RepositoryException
+   {
+
+      Node root = getTaskRootNode();
+      try
+      {
+         Node node = root.getNode(projectName);
+         return node;
+      }
+      catch (Exception e)
+      {
+         LOG.error(e);
+      }
+      return null;
+   }
+
+   public boolean removeProject(int id)
+   {
+      try
+      {
+         Node pRoot = getTaskRootNode();
+         Node pNode = getProjectNodeById(id);
+         pNode.remove();
+         pRoot.save();
+      }
+      catch (Exception e)
+      {
+         LOG.error(e);
+         return false;
+      }
+      return true;
+   }
+
+   protected Node getTaskRootNode() throws RepositoryException
+   {
+      SessionProvider sessionProvider = null;
+      Node node = null;
+      try
+      {
+         sessionProvider = SessionProvider.createSystemProvider();
+         ManageableRepository currentRepo = repositoryService.getCurrentRepository();
+         Session session = sessionProvider.getSession(currentRepo.getConfiguration().getDefaultWorkspaceName(), currentRepo);
+         Node rootNode = session.getRootNode();
+         if (!rootNode.hasNode(taskRootNode))
+         {
+            node = rootNode.addNode(taskRootNode);
+            rootNode.save();
+         }
+         else
+         {
+            node = rootNode.getNode(taskRootNode);
+         }
+
+      }
+      catch (Exception e)
+      {
+         LOG.error(e);
+      }
+      return node;
+   }
+
+   public Node getProjectNodeById(long id)
+   {
+      Node pRoot = null;
+      try
+      {
+         pRoot = getTaskRootNode();
+         String statement = "SELECT * FROM exo:project WHERE jcr:path LIKE '" +
+            pRoot.getPath() + "/%' AND idProject='" + id + "'";
+
+         Query query = pRoot.getSession().getWorkspace().getQueryManager().createQuery(statement, Query.SQL);
+         for (NodeIterator iter = query.execute().getNodes(); iter.hasNext(); )
+         {
+            Node p = iter.nextNode();
+            return p;
+         }
+      }
+      catch (RepositoryException e)
+      {
+         LOG.error(e);
+      }
+
+      return null;
+   }
+
+}
